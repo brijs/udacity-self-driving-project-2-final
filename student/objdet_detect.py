8ebc53da8762b46e70ece28eebbf53585ffef2de
Brijesh Shetty <brij.jhu@gmail.com>
2022-08-19 04:19:18 +0000

    Use mid-term code for objdet_detect

--- student/objdet_detect.py
+++ student/objdet_detect.py
@@ -62,6 +62,40 @@ def load_configs_model(model_name='darknet', configs=None):
         #######
         print("student task ID_S3_EX1-3")
 
+        configs.arch = 'fpn_resnet'
+        configs.saved_fn = 'fpn_resnet'
+        configs.model_path = os.path.join(parent_path, 'tools', 'objdet_models', 'resnet')
+        configs.pretrained_filename = os.path.join(configs.model_path, 'pretrained', 'fpn_resnet_18_epoch_300.pth')
+
+        # configs.cfgfile ?
+
+        configs.pin_memory = True
+        configs.distributed = False  # For testing on 1 GPU only
+
+        configs.input_size = (608, 608)
+        configs.hm_size = (152, 152)
+        configs.down_ratio = 4
+        configs.max_objects = 50
+        configs.conf_thresh = 0.5 # default value based on SFA: 0.2
+        configs.batch_size = 1
+
+        configs.imagenet_pretrained = False
+        configs.head_conv = 64
+        configs.num_classes = 3
+        configs.num_center_offset = 2
+        configs.num_z = 1
+        configs.num_dim = 3
+        configs.num_direction = 2  # sin, cos
+
+        configs.heads = {
+            'hm_cen': configs.num_classes,
+            'cen_offset': configs.num_center_offset,
+            'direction': configs.num_direction,
+            'z_coor': configs.num_z,
+            'dim': configs.num_dim
+        }
+        configs.num_input_features = 4
+
         #######
         ####### ID_S3_EX1-3 END #######     
 
@@ -118,6 +152,17 @@ def create_model(configs):
         ####### ID_S3_EX1-4 START #######     
         #######
         print("student task ID_S3_EX1-4")
+
+
+        try:
+            arch_parts = configs.arch.split('_')
+            num_layers = 18 # int(arch_parts[-1])
+        except:
+            raise ValueError
+
+        model = fpn_resnet.get_pose_net(num_layers=num_layers, heads=configs.heads,
+                                        head_conv=configs.head_conv,
+                                        imagenet_pretrained=configs.imagenet_pretrained)
 
         #######
         ####### ID_S3_EX1-4 END #######     
